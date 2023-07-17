81006355da3810a51c68a24fcf20ee6eda79015e
italo renan <italorenan225@gmail.com>
2023-07-17 10:58:47 -0300

    initial commit

---
+++ src/components/layoutImage/layoutImage.tsx
@@ -0,0 +1,107 @@
+import './layoutImage.css';
+
+export function LayoutImage() {
+	return (
+		<div className="container">
+			<div className="box">
+				<img src="/images/img1.png" />
+			</div>
+			<div className="box">
+				<img src="/images/img2.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img3.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img4.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img5.jpeg" />
+			</div>
+			<div className="box">
+				<img src="/images/img6.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img7.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img8.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img9.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img10.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img11.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img1.png" />
+			</div>
+			<div className="box">
+				<img src="/images/img2.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img3.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img4.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img5.jpeg" />
+			</div>
+			<div className="box">
+				<img src="/images/img6.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img7.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img8.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img9.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img10.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img11.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img1.png" />
+			</div>
+			<div className="box">
+				<img src="/images/img2.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img3.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img4.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img5.jpeg" />
+			</div>
+			<div className="box">
+				<img src="/images/img6.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img7.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img8.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img9.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img10.jpg" />
+			</div>
+			<div className="box">
+				<img src="/images/img11.jpg" />
+			</div>
+		</div>
+	);
+}
